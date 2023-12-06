e4bded5e866e59dca901426b613bc3b2e12b04a3
Ashley.Glenn <ashley.glenn@charter.com>
2023-12-06 17:13:16 -0600

    adding practice for interfaces

---
+++ src/AbstractionIndependentPractice/Customer.java
@@ -0,0 +1,8 @@
+package AbstractionIndependentPractice;
+
+public class Customer {
+    public static void main(String[] args) {
+        Product book = new Book();
+        book.setPrice(9.99);
+    }
+}
---
+++ src/AbstractionIndependentPractice/Product.java
@@ -0,0 +1,13 @@
+package AbstractionIndependentPractice;
+
+public interface Product {
+
+    double getPrice();
+    void setPrice(double price);
+
+    String getName();
+    void setName(String name);
+
+    String getColor();
+    void setColor(String color);
+}
